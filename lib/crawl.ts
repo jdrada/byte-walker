4879e626b3430620d2840b8d64be57dc1b53b43e
jdrada <j.ortizd@uniandes.edu.co>
2023-11-16 22:30:17 -0500

    Adding docs for crawl function

--- lib/crawl.ts
+++ lib/crawl.ts
@@ -4,6 +4,16 @@ import { getURLsFromHTML } from "./getURLsfromHTML";
 
 type PageObjectType = Record<string, number>;
 
+/**
+ * Recursively crawls web pages starting from a base URL. It keeps track of the number of visits
+ * to each page. The function only crawls pages within the same domain as the base URL and fetches
+ * new URLs from the HTML content of each page.
+ *
+ * @param {string} baseURL - The base URL to start crawling from.
+ * @param {string} [currentURL=baseURL] - The current URL being crawled. Defaults to the base URL.
+ * @param {PageObjectType} [pages={}] - An object to keep track of the number of visits to each page.
+ * @returns {Promise<PageObjectType>} A promise that resolves to an object with the count of visits per page.
+ */
 export const crawlPage = async (
   baseURL: string,
   currentURL: string = baseURL,
@@ -14,35 +24,34 @@ export const crawlPage = async (
     const baseURLObject = new URL(baseURL);
     const currentURLObject = new URL(currentURL);
 
+    // Check if the current URL is from the same domain as the base URL.
     if (baseURLObject.hostname !== currentURLObject.hostname) return pages;
 
-    const normalizedCurrentURL = normalizeURL(currentURL!);
+    const normalizedCurrentURL = normalizeURL(currentURL);
 
+    // Increment the count if the page has been visited before, or set it to 1.
     if (pages[normalizedCurrentURL] > 0) {
       pages[normalizedCurrentURL]++;
       return pages;
-    }
-
-    if (currentURL === baseURL) {
-      pages[normalizedCurrentURL] = 0;
     } else {
       pages[normalizedCurrentURL] = 1;
     }
 
-    console.log(`crawling ${currentURL}`);
+    console.log(`Crawling ${currentURL}`);
     const response = await fetch(currentURL);
     const contentType = response.headers.get("content-type");
 
+    // Process only if the content type is HTML.
     if (contentType?.includes("text/html")) {
       const newURLs = getURLsFromHTML(await response.text(), baseURL);
       for (const nextURL of newURLs) {
         crawledPages = await crawlPage(baseURL, nextURL, pages);
       }
     } else {
-      throw new Error("Response is not text/html");
+      console.log("Response is not text/html -- Ommiting");
     }
   } catch (error) {
-    console.log(`Something went wrong! ${error}`);
+    console.error(`Something went wrong! ${error}`);
   }
   return crawledPages;
 };
--- lib/report.ts
+++ lib/report.ts
@@ -1,14 +1,26 @@
 import fs from "fs";
+
+/**
+ * Writes a report to a file in the 'reports' directory. The report contains
+ * data from the 'pages' object, which is serialized into JSON format. The
+ * filename is generated based on the current timestamp to ensure uniqueness.
+ *
+ * @param {Record<string, number>} pages - An object containing page-related data,
+ *                                         where the key is the page identifier and
+ *                                         the value is some numeric data (e.g., page views).
+ */
 export const printReport = (pages: Record<string, number>) => {
   console.log("----- Printing Report ------");
   try {
+    // Write the report to a file in the 'reports' directory.
     fs.writeFileSync(
       `./reports/${Date.now()}.json`,
       JSON.stringify(pages),
       "utf8"
     );
-    console.log("----- Report printed succesfully! ------");
+    console.log("----- Report printed successfully! ------");
   } catch (error) {
+    // Log any errors encountered during file writing.
     console.error(error);
   }
 };
